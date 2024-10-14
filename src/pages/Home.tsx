4bbad888fd5dd43a1732ecb0d88e66d82e8f300b
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-15 00:07:13 +0530

    oneway

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ export function Homepage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold mb-6">VideoCall</h1>
+     
       <div className="flex flex-col md:flex-row gap-8">
         <div className="bg-white shadow-md rounded-lg p-6 w-full md:w-1/2" onClick={()=>nav("/s")} >
           <button>
